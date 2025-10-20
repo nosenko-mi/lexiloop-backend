b49cd9b0b35daee76d0243fd3d58657bf8507bbb
Mykola Nosenko <nosenkomi02@gmail.com>
2025-10-20 20:33:03 +0300

    use proper database instead of sqlite

--- app/db/database.py
+++ app/db/database.py
@@ -1,22 +1,47 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+from app.utils.aws_secrets import get_aws_secret 
 
-QUIZZES_DATABASE_URL = "sqlite:///./data/lexiloop.db"
-TEXTS_DATABASE_URL = "sqlite:///./data/texts.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+# for local development
+load_dotenv()
 
-engine = create_engine(
-    QUIZZES_DATABASE_URL, 
-    connect_args={"check_same_thread": False} # sqlite only
-)
+if os.environ.get("USE_AWS_SECRETS") == "true":
+    
+    print("Using AWS RDS and Secrets Manager")
+    
+    db_creds = get_aws_secret("DB_SECRET_NAME")
+    
+    DB_USER = db_creds['username']
+    DB_PASS = db_creds['password']
+    DB_HOST = db_creds['host']
+    DB_PORT = db_creds.get('port', 5432)
+    DB_NAME = db_creds['dbname']
+    
+    SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+else:
+    
+    print("Using local Docker PostgreSQL database")
+    
+    DB_USER = os.environ.get("DB_USER")
+    DB_PASS = os.environ.get("DB_PASS")
+    DB_HOST = os.environ.get("DB_HOST")
+    DB_PORT = os.environ.get("DB_PORT", 5432)
+    DB_NAME = os.environ.get("DB_NAME")
+
+    if not all([DB_USER, DB_PASS, DB_HOST, DB_NAME]):
+        raise ValueError("Missing local database environment variables. Did you create .env?")
+
+    SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-texts_engine = create_engine(
-    TEXTS_DATABASE_URL, 
-    connect_args={"check_same_thread": False} # sqlite only
-)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-TextsSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=texts_engine)
 
 Base = declarative_base()
